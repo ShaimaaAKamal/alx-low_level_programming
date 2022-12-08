891baf146f311f165514bc3979e677cc2d7bb23e
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-08 05:43:36 -0800

    print to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function to print a string
+ *
+ * Return: always 1
+ */
+int main(void)
+{
+	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
