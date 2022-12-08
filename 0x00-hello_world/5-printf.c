749dc02582b4107b39be23105084a68543ec1d86
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-08 05:13:00 -0800

    print f hello

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function to print a string
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("%s\n","with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
