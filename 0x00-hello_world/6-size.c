b576dae5d38b0311b645da52c51ee5af2a5d034f
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-08 05:27:32 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - function to print data size
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i;
+	int x;
+	long int y;
+	long long int z;
+	float r;
+	printf("Size of a char: %lu byte(s)\n",(unsigned long) sizeof(i));
+	printf("Size of a int: %lu byte(s)\n",(unsigned long) sizeof(x));
+	printf("Size of a long int: %lu byte(s)\n",(unsigned long) sizeof(y));
+	printf("Size of a long long int: %lu byte(s)\n",(unsigned long) sizeof(z));
+	printf("Size of a float: %lu byte(s)\n",(unsigned long) sizeof(r));
+	return (0);
+}
