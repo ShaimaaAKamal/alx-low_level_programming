7fa62d4b7187b83ec5495a3c73ade89e1396753e
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-09 11:55:05 -0800

    two digits com.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int;
+
+	for (i = 48 ; i <= 57 ; i++)
+	{
+		putchar(i);
+		for( j = i+1 ; J <=57 ; j++)
+		{
+			putchar(j);
+			 if (i != 57 && j != 57)
+                	{
+                        	putchar(',');
+                        	putchar(' ');
+                	}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
