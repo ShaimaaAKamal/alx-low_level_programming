8f36867479183a47a74f528f0252af78c3e6b1cc
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-09 10:10:37 -0800

    update betty styling

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
  * main - function used to check whatever number is positive or nagative
- *
+ * Return : always 0
  */
 
 int main(void)
@@ -14,10 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n  > 0)
-		printf("%d is positive\n",n);
+		printf("%d is positive\n", n);
 	else if (n < 0)
-		printf("%d is negative\n",n);
-	else    printf("%d is zero\n",n);
+		printf("%d is negative\n", n);
+	else    printf("%d is zero\n", n);
 
 	return (0);
 
