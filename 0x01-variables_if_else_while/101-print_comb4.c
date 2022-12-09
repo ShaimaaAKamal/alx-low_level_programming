af2365968ffdf57b554654ff4f4377dfca13e165
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-09 12:18:38 -0800

    3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48 ; i <= 57 ; i++)
+	{
+		for (j = i + 1 ; j <= 57 ; j++)
+		{
+			for (k = i + 2 ; k <= 57 ; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != 55)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
