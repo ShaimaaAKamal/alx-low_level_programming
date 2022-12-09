445f29c8b25ca23e96309aee2e58d39a915f012c
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-09 11:58:57 -0800

    update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,11 +11,12 @@ int main(void)
 
 	for (i = 48 ; i <= 57 ; i++)
 	{
-		putchar(i);
+		
 		for( j = i+1 ; j <=57 ; j++)
 		{
+			putchar(i);
 			putchar(j);
-			 if (i != 57 && j != 57)
+			if (i != 57 && j != 57)
                 	{
                         	putchar(',');
                         	putchar(' ');
