a4239b92143df4e094c88eeb00f65e51da64416b
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-13 11:44:06 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs  - Check description
+ * Description: It calculate abs of a number.
+ * @n: input number
+ * Return: abs of a number.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return ((-1 * n));
+	}
+}
