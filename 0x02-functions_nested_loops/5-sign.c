a0bac8db634b703dee846feac97fe89ee9c14565
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-13 11:37:32 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign  - Check description
+ * Description: It checks sign of a number.
+ * @n: input number
+ * Return: 1 if psoitive , 0 if zer0 and -1 if negative.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		-putchar('-');
+		return (-1);
+}
