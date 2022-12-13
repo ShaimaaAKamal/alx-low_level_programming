db32e42e79977750ab984aa637a95de06d41e155
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-13 13:27:19 -0800

    update

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,13 +13,13 @@ int main(void)
 
 	fibonacci[0] = 1;
 	fibonacci[1] = 2;
-	i=2;
+	i = 2;
 	printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
 
-	while (i <= 97)
+	while (i <= 96)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-		if (i == 97)
+		if (i == 96)
 			printf("%ld\n", fibonacci[i]);
 		else
 			printf("%ld, ", fibonacci[i]);
