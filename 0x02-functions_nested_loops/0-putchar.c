3e0ea76ed95ba9f1f061a5964d76f4c930591d5c
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-13 10:08:04 -0800

    ypdate __putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
-
+#include <main.h>
 /**
  * main - prints a string
  *
  * Return : 0 alwats
  */
-int main()
+int main(void)
 {
-	printf("%s\n","_putchar");
+	char name[8] = '_putchar';
+	int i;
+
+	for (i = 0; i < 8 ; i++)
+		_putchar(name[i]);
+	_putchar("\n");
 	return (0);
 }
