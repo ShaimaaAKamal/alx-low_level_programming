443b7f846551b669eea97a0fb97e4eb6b3788783
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-13 10:59:49 -0800

    print lower

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Check description
+ * Description: It prints loercase characters, fed by a new line.
+ * Return: Nothing.
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+		_putchar(i);
+	_putchar('\n');
+}
