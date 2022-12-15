37f9aa140f80a41bdc90798832961dae916c95ef
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-15 09:08:03 -0800

    print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - A function that checks for uppercase character.
+ * Return: Nothing
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
