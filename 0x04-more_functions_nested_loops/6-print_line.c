a9bd0eae7336986e373a3252be564cbbcd04332e
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-15 13:20:55 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - A function that checks for uppercase character.
+ * @n: int number
+ * Return: Nothing
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
