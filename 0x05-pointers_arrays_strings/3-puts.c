6a74864a238efe0378f5e711d0dcbe4752baaee1
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-19 02:21:58 -0800

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdbool.h>
+/**
+ * _puts - function used to print string
+ * @s: is an char  pointer
+ * Return: Nothing
+ */
+int _puts(char *s)
+{
+	bool end = true;
+
+	while (end)
+	{
+		if (*s != '\0')
+			_putchar(*s)
+		else
+			end = false;
+		s++;
+	}
+	_putchar('\n');
+}
